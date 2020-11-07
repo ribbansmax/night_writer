5926749bf959e0c50c84d307a43e794b16ebfc9b
Max Ribbans <ribbans.max@gmail.com>
2020-11-07 12:01:21 -0500

    Add split_english

--- lib/translation.rb
+++ lib/translation.rb
@@ -13,4 +13,13 @@ class Translation
   def characters
     reader.characters
   end
+
+  def split_english
+    lines = reader.lines
+    english = []
+    lines.each do |line|
+      english << line.chars
+    end
+    english.flatten
+  end
 end
