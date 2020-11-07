592480f816122b8ae881c215b97b40f838dcf250
Max Ribbans <ribbans.max@gmail.com>
2020-11-06 20:20:41 -0500

    Test it exists and has attributes

--- test/reader_test.rb
+++ test/reader_test.rb
@@ -4,5 +4,12 @@ require 'mocha/minitest'
 require './lib/reader'
 
 class ReaderTest < Minitest::Test
+  def test_it_exists_and_has_attributes
+    origin = "dummy.txt"
+    reader = Reader.new(origin)
 
+    expected = "hello world"
+
+    assert_equal expected, reader.first_line
+  end
 end
