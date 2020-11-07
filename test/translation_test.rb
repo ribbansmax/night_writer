2bb05395e04394d25f698c22015bd6fe5fea4e1f
Max Ribbans <ribbans.max@gmail.com>
2020-11-06 20:52:36 -0500

    Refactor to instance Reader from initialize and remove origin attr

--- test/translation_test.rb
+++ test/translation_test.rb
@@ -2,19 +2,20 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require 'mocha/minitest'
 require './lib/translation'
+require './lib/reader'
 
 class TranslationTest < Minitest::Test
   def test_it_exists_and_has_attributes
-    file_to_read = "messages.txt"
+    file_to_read = "dummy.txt"
     file_to_make = "braille.txt"
     translation = Translation.new(file_to_read, file_to_make)
 
-    assert_equal file_to_read, translation.origin
     assert_equal file_to_make, translation.destination
+    assert_instance_of Reader, translation.reader
   end
 
   def test_it_can_detect_english
-    file_to_read = "messages.txt"
+    file_to_read = "dummy.txt"
     file_to_make = "braille.txt"
     translation = Translation.new(file_to_read, file_to_make)
 
