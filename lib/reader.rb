d7eebe26498a326f981159a0548eae2f629515ac
Max Ribbans <ribbans.max@gmail.com>
2020-11-06 20:42:22 -0500

    Add characters method that counts all characters

--- lib/reader.rb
+++ lib/reader.rb
@@ -1,6 +1,18 @@
 class Reader
-  attr_reader :path
+  attr_reader :path, :lines, :first_line
   def initialize(path)
     @path = "./data/" + path
+    line_maker
+    @first_line = lines[0]
+  end
+
+  def line_maker
+    @lines = File.readlines(path, chomp: true)
+  end
+
+  def characters
+    lines.sum do |line|
+      line.length
+    end
   end
 end
