ee5b28570abe482bd272fc302e3d526a042bf998
Max Ribbans <ribbans.max@gmail.com>
2020-11-07 13:29:10 -0500

    Add split_braille method - needs to be broken out into multiple methods soon

--- lib/translation.rb
+++ lib/translation.rb
@@ -22,4 +22,28 @@ class Translation
     end
     english.flatten
   end
+
+  def split_braille
+    lines = reader.lines
+    times = lines.length / 3
+    braille = []
+    ticker = 0
+    while ticker < times do
+      num_chars = lines[(ticker +1) * 3 -1].length / 2
+      characters = 0
+      until num_chars == 0 do
+        braille[characters] = []
+        line_num = 0
+        3.times do
+          braille[characters] << lines[line_num].slice!(0..1)
+          line_num += 1
+        end
+        braille[characters] = braille[characters].join
+        characters += 1
+        num_chars -=1
+      end
+      ticker += 1
+    end
+    braille
+  end
 end
